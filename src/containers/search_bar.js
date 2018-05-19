5b6d2e8187319c1bc6f12722a0bfe714377ed37c
krentavlad <39372609+krentavlad@users.noreply.github.com>
2018-05-19 14:25:05 +0200

    Fix bug

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -5,7 +5,7 @@ import {fetchWeather} from "../actions/index"
 class SearchBar extends Component {
     constructor(props) {
         super(props)
-        this.state = {term:"VINNYTSYA"}
+        this.state = {term:""}
         this.onFormSubmit=this.onFormSubmit.bind(this)
     }
     onInputChange = (event) => {
